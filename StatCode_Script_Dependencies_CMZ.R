852d9ec0aeb10244b61807c397573374cdd3796a
Carlos-Martinez-Zamora <martinez.zamora.car@gmail.com>
2026-03-13 02:00:41 +0100

    Add files via upload

---
+++ StatCode_Script_Dependencies_CMZ.R
@@ -0,0 +1,17 @@
+paquetes <- c(
+  "shiny",           
+  "readxl",         
+  "openxlsx",       
+  "stats",          
+  "ggplot2",        
+  "ggbreak",        
+  "dplyr",          
+  "DescTools",      
+  "car",          
+  "rstatix",       
+  "broom",       
+  "PMCMRplus",      
+  "nortest"       
+)
+
+install.packages(paquetes)
